a2c8ee83b8bdc8b0b5c5aee95bc9fb022347ac30
yousuf rehman <yousufrehman543@gmail.com>
2020-07-27 06:40:42 +0500

    Version 2-Basic Client Server Model
    -Cannot again reconnect
    -Bug: After connected, if the client disconnect cannot get it

--- 1-Client_Server_Model/client.c
+++ 1-Client_Server_Model/client.c
@@ -1,19 +1,42 @@
-#include<stdio.h>
+#include<stdio.h>   //printf
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
-#include<string.h>
-#include<fcntl.h> // for open
-#include<unistd.h> // for close
-#include <stdlib.h>
+#include<string.h> //memset, strcmp
+#include<stdlib.h> //malloc
+#include<fcntl.h> //for open
+#include<unistd.h> //for close
+#include<time.h> //To use time library of C
+#include<signal.h>
 
 #define DEST_IP "192.168.10.21"
 #define DEST_PORT 4200
 
+int sockfd;
+
+/* Signal Handler for SIGINT */
+void sigintHandler(int sig_num)
+{
+  /* Reset handler to catch SIGINT next time. SIGINT when ctrl+c pressed*/
+  signal(SIGINT, sigintHandler);
+  printf("\nYou have pressed Ctrl+c, safe shutting down client!!!\n");
+  fflush(stdout);
+  close(sockfd);
+  exit(0);
+}
+
+void delay(int milli_seconds)
+{
+  // Storing start time 
+  clock_t start_time = clock();
+  // looping till required time is not achieved 
+  while (clock() < start_time + milli_seconds) ;
+}
+
 void main(){
-  printf(".......CLIENT.........\n");
-  int sockfd;
+  signal(SIGINT, sigintHandler);
+  printf("\n.......CLIENT.........\n");
   struct sockaddr_in dest_addr;
   
   sockfd = socket(PF_INET, SOCK_STREAM, 0);
@@ -29,12 +52,35 @@ void main(){
   dest_addr.sin_addr.s_addr = inet_addr(DEST_IP);
   memset(&dest_addr.sin_zero, '\0',8);
   
-  printf("Connecting....");
-  connect(sockfd, (struct sockaddr*)&dest_addr, sizeof(struct sockaddr));
-  //printf("%s", "connect error");
-  printf("Connected");
+  printf("Connecting....\n");
+  if(connect(sockfd, (struct sockaddr*)&dest_addr, sizeof(struct sockaddr)) == -1)
+    printf("connect error\n");
+  else
+    printf("Connected\n");
+
   int ByteSend = 20;
-  char *SendMsg = "Hi";
-  send(sockfd, SendMsg, ByteSend, 0);
+  char *SendMsg = (char *)malloc(ByteSend*sizeof(char));
+  while(1){
+    printf("\nEnter an Msg: ");
+    scanf("%s", SendMsg);
+    ByteSend = send(sockfd, SendMsg, ByteSend, 0);
+    
+    if(ByteSend > 0){
+      printf("ByteSent = %d bytes\n", ByteSend);
+      
+      if(!strcmp(SendMsg, "end")){
+        printf("\nshutting down...\n");
+        break;
+      }
+
+    }
+    else if(ByteSend == -1){
+      printf("\nConnection lost...\n");
+      break;
+    }
+
+    delay(100);//wait 100ms
+  }
+  
   close(sockfd);
 }
--- 1-Client_Server_Model/server.c
+++ 1-Client_Server_Model/server.c
@@ -3,22 +3,50 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
-#include<string.h> //memset
+#include<string.h> //memset, strcmp
 #include<stdlib.h> //malloc
-#include<fcntl.h> // for open
-#include<unistd.h> // for close
+#include<fcntl.h> //for open
+#include<unistd.h> //for close
+#include<time.h> //To use time library of C
+#include<signal.h>
+
 
 #define MYPORT 4200
 #define MyIP "192.168.10.21"
 #define BACKLOG 10
 
+int sockfd;//socket file descriptor
+int new_fd;
+/* Signal Handler for SIGINT */
+void sigintHandler(int sig_num) 
+{ 
+  /* Reset handler to catch SIGINT next time. SIGINT when ctrl+c pressed*/
+  signal(SIGINT, sigintHandler);
+  printf("\nYou have pressed Ctrl+c, safe shutting down server!!!\n"); 
+  fflush(stdout);
+  close(new_fd);
+  close(sockfd);
+  exit(0);
+} 
+
+void delay(int milli_seconds) 
+{  
+  // Storing start time 
+  clock_t start_time = clock(); 
+  // looping till required time is not achieved 
+  while (clock() < start_time + milli_seconds) ; 
+} 
+
+
+
 int main(int argc, char *argv[]){
-  printf(".......SERVER.........");
+  signal(SIGINT, sigintHandler);
+  printf("\n.......SERVER.........\n");
   int error;
-  int sockfd; //socket file descriptor
   struct sockaddr_in server_addr;
+  
   //getting socket
-  printf("Creating Socket....");
+  printf("Creating Socket....\n");
   sockfd = socket(AF_INET, SOCK_STREAM,0);
   if(sockfd == -1){
     printf("socket creation failed...\n");
@@ -26,11 +54,14 @@ int main(int argc, char *argv[]){
   }
   else
     printf("Socket successfully created..\n");
+  
+  
   //putting values in sockaddr_in
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(MYPORT);
   server_addr.sin_addr.s_addr = inet_addr(MyIP);
   memset(&server_addr.sin_zero, '\0', 8);//zero all values
+  
   
   printf("Binding Port = %d\n", MYPORT); 
   error = bind(sockfd,(struct sockaddr*)&server_addr,sizeof(struct sockaddr));
@@ -39,26 +70,53 @@ int main(int argc, char *argv[]){
     exit(0);
   }
   
-  printf("Listening to Port = %d\n", MYPORT);  
+  
+  
   error = listen(sockfd, BACKLOG);
   if(error == -1){
-    printf("%s", "listen error");
-    exit(0);
+    printf("\nlisten error\n");
   }
+  else
+    printf("Listening to Port = %d\n", MYPORT);
 
-  int new_fd;
+  
   struct sockaddr_in client_addr;
   int sin_size = sizeof(struct sockaddr);
 
   new_fd = accept(sockfd,(struct sockaddr*)&client_addr, &sin_size);
 
-  if(new_fd == -1)
-    printf("%s", "accept error");
+  if(new_fd == -1){
+    printf("\naccept error\n");
+  }
+  else 
+    printf("Connection Accepted from  IP = %s, Port = %d\n", inet_ntoa(client_addr.sin_addr), client_addr.sin_port);
 
-  char *rcvMsg = (char *)malloc(20*sizeof(char));
   int ByteRecv = 20;
-  ByteRecv = recv(new_fd, rcvMsg, ByteRecv, 0);
-  printf("%s", rcvMsg);
+  char *rcvMsg = (char *)malloc(ByteRecv*sizeof(char));
+  
+  while(1){
+    ByteRecv = 20;
+    *rcvMsg = '\0';
+    ByteRecv =  recv(new_fd, rcvMsg, ByteRecv, 0);
+    
+    if(ByteRecv > 0){
+      printf("\nMsg = %s\n", rcvMsg);
+      printf("ByteReceived = %d bytes\n", ByteRecv);
+      
+      if(!strcmp(rcvMsg, "end")){
+        printf("\nshutting down...\n");
+        break;
+      }
+
+    }
+    
+    else if(ByteRecv == -1){
+      printf("\nConnection Lost....\n");
+    }
+    
+    delay(100);//wait 100ms
+  }
+
   close(new_fd);
   close(sockfd);
   
